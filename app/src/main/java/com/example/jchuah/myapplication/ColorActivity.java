971cb5a3cf49cbf0853511bce6126b884a991014
Aahuja <avikahuja100@GMAIL.COM>
2015-10-20 13:01:49 -0500

    I added code that makes the buttons on screen one work, on the Main Activity and Color Activity

--- app/src/main/java/com/example/jchuah/myapplication/ColorActivity.java
+++ app/src/main/java/com/example/jchuah/myapplication/ColorActivity.java
@@ -1,45 +1,26 @@
 package com.example.jchuah.myapplication;
 
-import android.content.Intent;
-import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    static String tag= "com.example.jchuah.myapplication.MainActivity";
+public class ColorActivity extends AppCompatActivity {
 
-
-    Bundle sendBundle = new Bundle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_color);
+        Bundle groceries = getIntent().getBundleExtra("groceries");
+        Log.i("groceries", groceries.getString("name"));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_color, menu);
         return true;
-    }
-
-    public void onNextClick(View source){
-        //Toast.makeText(this,"Houdy", Toast.LENGTH_LONG ).show();
-        Log.i(tag, "launching Color Activity");
-        Intent colorActivityIntent = new Intent(this, ColorActivity.class);
-
-        sendBundle.putString("name", "Tom");
-
-        colorActivityIntent.putExtra("groceries", sendBundle);
-
-        startActivity(colorActivityIntent);
     }
 
     @Override
